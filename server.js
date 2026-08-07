506c6444b70c5aa79b9a9566207fad0124952bd1
regisagnikpe2002-ai <regisagnikpe2002@gmail.com>
2026-08-07 17:09:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,3 +9,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`OMNIPAY backend running on port ${PORT}`);
 });
+app.get('/', (req, res) => {
+  res.send('OMNIPAY API is running');
+});
