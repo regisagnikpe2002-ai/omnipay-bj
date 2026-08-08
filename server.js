2218a398c85d9d55838f6eb1c0e5637e883d175d
regisagnikpe2002-ai <regisagnikpe2002@gmail.com>
2026-08-08 20:09:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.use('/auth', authRoutes);
 
 // Route de test
 app.get('/', (req, res) => {
-  res.send('OMNIPAY API fonctionne ✔');
+    res.send('OMNIPAY API fonctionne ✔');
 });
 
 // Démarrage du serveur
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Serveur OMNIPAY démarré sur le port ${PORT}`);
+    console.log(`Serveur OMNIPAY démarré sur le port ${PORT}`);
 });
