8368699ce518a615d013bc601602be83a3d0d6f4
regisagnikpe2002-ai <regisagnikpe2002@gmail.com>
2026-08-30 17:10:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+require('dotenv').config();
 
+const app = express();
+
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
+// Routes
 const authRoutes = require('./routes/auth');
-
 app.use('/auth', authRoutes);
 
+// Route test
 app.get('/', (req, res) => {
-    res.send('OMNIPAY API fonctionne ✓');
+  res.send('OMNIPAY API fonctionnel');
 });
 
 // Render impose son propre port
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`OMNIPAY backend running on port ${PORT}`);
+  console.log(`OMNIPAY backend running on port ${PORT}`);
 });
