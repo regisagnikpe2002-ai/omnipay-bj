6df3483b02bcdc9504084f59f929a4783eba5a9b
Regis Agnikpe <300570797+regisagnikpe2002-ai@users.noreply.github.com>
2026-09-24 08:55:24 +0200

    Fix: accepter les webhooks PayDunya en urlencoded

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 const authRoutes = require('./routes/auth');
